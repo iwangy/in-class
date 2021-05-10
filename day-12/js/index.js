02b94b9e258431a189de81e0c20d6b236b71d265
iwangy <35878784+iwangy@users.noreply.github.com>
2021-05-09 22:40:24 -0700

    complete d11, d12

--- day-12/js/index.js
+++ day-12/js/index.js
@@ -4,11 +4,26 @@
 // Class Puppy
 class Puppy {
     // Constructor: takes in an img url, and a sound
-
+    constructor(
+        img = "https://cdn.shopify.com/s/files/1/0272/4770/6214/articles/when-do-puppies-start-walking.jpg?v=1593020034",
+        sound = "WOOF!"
+        ) {
+        this.img = img;
+        this.sound = sound;
+    }
     // Method for "speaking" using responsiveVoice
-
+    bark() {
+        responsiveVoice.speak(this.sound);
+    }
     // Render a Div that you can click on to bark
-
+    render() {
+        let puppyCard = $('<div>');
+        puppyCard.attr('title', `puppy that says ${this.sound}`);
+        puppyCard.attr('class', 'puppyCard col-sm-4');
+        puppyCard.css('background-image', `url(${this.img})`);
+        puppyCard.click(() => this.bark());
+        return puppyCard;
+    }
 }
 
 
@@ -52,23 +67,48 @@ class PuppyForm {
 // Class for the app
 class PuppyApp {
     // Constructor: takes in a parent element and list of puppies
-
+    constructor(parentElement, puppyList) {
+        this.parentElement = parentElement;
+        this.puppyList = puppyList;
+    }
     // Add puppy: pushes new data into list of puppies and re-renders the app
-
+    addPuppy(img, sound) {
+        console.log(this);
+        this.puppyList.push({
+            url: img,
+            sound: sound
+        });
+        this.render();
+    }
 
     // Render;
     render() {
         // Empty parent element
-
+        this.parentElement.empty();
 
         // Create and render a new form
+        let form = new PuppyForm(this.addPuppy.bind(this));
+        this.parentElement.append(form.render());
 
         // Append puppy list element to parent (in a wrapper div)
-
+        let puppyWrapper = $('<div class="row">');
+        this.parentElement.append(puppyWrapper);
+        this.puppyList.map((puppyInfo) => {
+            let newPuppy = new Puppy(puppyInfo.url, puppyInfo.sound);
+            puppyWrapper.append(newPuppy.render());
+        })    
     }
+
 }
 
 // Create a new app with a single puppy
-
+let app = new PuppyApp(
+    $('#content'),
+    [{
+        url: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=1.00xw:0.669xh;0,0.190xh&resize=640:*",
+        sound: "bark"
+    }]
+)
 
 // Render the app
+app.render();
